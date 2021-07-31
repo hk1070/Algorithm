c4d37d3a1cffcdad3eea84218fe15b86df568d64
shk07 <seohk107@gmail.com>
2021-07-31 23:34:23 +0900

    Day1

---
+++ section1/3_연필개수.js
@@ -0,0 +1,26 @@
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+/* 
+      Math.ceil(2.111) -> 3
+      Math.floor(2.111) -> 2
+      Math.round(2.6) -> 3 
+      Math.sqrt(64) -> 8 
+      */
+
+function solution(n) {
+  let answer = Math.ceil(n / 12);
+  return answer;
+}
+
+console.log(solution(25));
+console.log(solution(178));
+console.log(solution(211));
+
+function solution2(n) {
+  let answer = Math.floor(n / 12) + 1;
+  return answer;
+}
+
+console.log(solution2(25));
+console.log(solution2(178));
+console.log(solution2(211));
