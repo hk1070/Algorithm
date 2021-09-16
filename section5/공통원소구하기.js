c2994c82e73edceceab42d8456a0ad2f5cb06abe
shk07 <seohk107@gmail.com>
2021-09-16 22:42:58 +0900

    0916

--- section5/공통원소구하기.js
+++ section5/공통원소구하기.js
@@ -16,3 +16,5 @@ function solution(arr1, arr2) {
 let a = [1, 3, 9, 5, 2];
 let b = [3, 2, 5, 7, 8];
 console.log(solution(a, b));
+
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
